bc5307ca3d1e71725176d94474a226f669718ec0
Tom Shelley <tom@tasman.ai>
2024-01-19 11:01:17 +0000

    🐛 fix hardcoded snowflake notation in int tests

--- integration_tests/models/example/stg_conversion_events.sql
+++ integration_tests/models/example/stg_conversion_events.sql
@@ -1,6 +1,6 @@
 select
     {{ dbt.safe_cast("event", "string") }} as conversion_event_id,
-    timestamp::timestamp as conversion_timestamp,
+    event_timestamp as conversion_timestamp,
     {{ dbt.safe_cast("user", "string") }} as user_id,
     {{ dbt.safe_cast("type", "string") }} as conversion_type
 from
--- integration_tests/models/example/stg_touch_events.sql
+++ integration_tests/models/example/stg_touch_events.sql
@@ -1,6 +1,6 @@
 select
     {{ dbt.safe_cast("event", "string") }} as touch_event_id,
-    timestamp::timestamp as touch_timestamp,
+    event_timestamp as touch_timestamp,
     {{ dbt.safe_cast("user", "string") }} as user_id,
     {{ dbt.safe_cast("channel", "string") }} as touch_channel
 from

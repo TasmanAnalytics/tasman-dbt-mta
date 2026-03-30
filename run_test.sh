026f7629981a5c106730dc178f508cc2f77fc90f
Jurri Gerretsen <62351599+jurrigerretsen@users.noreply.github.com>
2026-03-30 09:35:35 +0200

    feat: update run test script to accept target variables

--- run_test.sh
+++ run_test.sh
@@ -7,6 +7,8 @@
 BLUE='\033[1;34m'
 END='\033[0m'
 
+TARGET=${1:-snowflake-ci}
+
 echo -e "\n${BLUE}dbt executable location${END}"
 echo $(which dbt)
 
@@ -17,16 +19,16 @@ dbt --version
 cd integration_tests
 export DBT_PROFILES_DIR=.
 
-echo -e "\n${BLUE}dbt debug${END}"
-dbt debug --target snowflake-ci
+echo -e "\n${BLUE}dbt debug (${TARGET})${END}"
+dbt debug --target "$TARGET"
 
 echo -e "\n${BLUE}dbt deps${END}"
-dbt deps --target snowflake-ci || exit 1
+dbt deps --target "$TARGET" || exit 1
 
 # build will seed, run, and test
 echo -e "\n${BLUE}dbt build (full refresh)${END}"
-dbt build --target snowflake-ci --full-refresh || exit 1
+dbt build --target "$TARGET" --full-refresh || exit 1
 
 # build again for incremental models
 echo -e "\n${BLUE}dbt build (incremental)${END}"
-dbt build --target snowflake-ci || exit 1
+dbt build --target "$TARGET" || exit 1
